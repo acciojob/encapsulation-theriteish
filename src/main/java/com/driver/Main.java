c08cc2bcd4d0df7db8f2ed92632f38915f4d4c32
Ritesh RAj <114146850+theriteish@users.noreply.github.com>
2023-07-18 14:00:27 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Ritesh");
+        String name = obj.getName();
+        System.out.println(name);
+    }
   
 }
